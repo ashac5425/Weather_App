bb2cf87d3d0b0014e2a5918f8b70bcc66393ef89
ashac5425 <ashac5425@gmail.com>
2025-05-02 15:51:47 -0500

    removed comment code

--- static/script.js
+++ static/script.js
@@ -1,23 +1,4 @@
 
-// function fetchWeather() {
-//     const city = document.getElementById('city').value;
-//     fetch(`/getweather?city=${city}`)
-//         .then(response => response.json())
-//         .then(data => {
-//             const resultDiv = document.getElementById('weatherResult');
-//             if (data.error) {
-//                 resultDiv.innerHTML = `<p>Error: ${data.error}</p>`;
-//             } else {
-//                 resultDiv.innerHTML = `
-//                     <p><strong>City:</strong> ${data.city}</p>
-//                     <p><strong>Temperature:</strong> ${data.temperature}</p>
-//                     <p><strong>Humidity:</strong> ${data.humidity}</p>
-//                     <p><strong>Wind:</strong> ${data.wind}</p>
-//                 `;
-//             }
-//         })
-//         .catch(error => console.error('Error:', error));
-// }
 
 
 // Fetch weather for a city
